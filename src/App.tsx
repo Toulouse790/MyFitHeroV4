f460b96bd166e1e055bc5843acf30c56f74dfef6
Toulouse790 <cstophe0@gmail.com>
2025-06-23 13:31:10 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Index from './pages/Index';
 
-export default App;
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -29,7 +16,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
