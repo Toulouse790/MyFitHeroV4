601a1fcb9fd13f359f3bfc9006178813103e83c5
Toulouse790 <cstophe0@gmail.com>
2025-06-23 13:24:10 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,18 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from './pages/Index';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 const queryClient = new QueryClient();
 
 const App = () => (
