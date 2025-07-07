c8ae4b4a51fd67c83a201dd52450970e0dc171f0
Toulouse790 <cstophe0@gmail.com>
2025-07-07 09:44:50 +0200

    Delete client/src/pages/Dashboard.tsx

--- client/src/pages/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SmartDashboard from '@/components/SmartDashboard';
-
-const Dashboard: React.FC = () => {
-  return <SmartDashboard />;
-};
-
-export default Dashboard;
