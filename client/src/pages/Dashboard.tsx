8f8159c1634c6c1ee4fbdf31773de1e73f967f93
Toulouse790 <cstophe0@gmail.com>
2025-07-07 09:07:30 +0200

    Create Dashboard.tsx

---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SmartDashboard from '@/components/SmartDashboard';
+
+const Dashboard: React.FC = () => {
+  return <SmartDashboard />;
+};
+
+export default Dashboard;
