c12281164a960a9f5ba68fa76a3fc2fc818c3849
Toulouse790 <cstophe0@gmail.com>
2025-07-25 13:12:11 +0200

    Update tailwind.config.ts

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss";
 export default {
   darkMode: ["class"],
   content: [
-    "./index.html", 
+    "./Index.html", 
     "./src/**/*.{js,jsx,ts,tsx}",
     "./src/**/*.{html,js,ts,jsx,tsx}"
   ],
