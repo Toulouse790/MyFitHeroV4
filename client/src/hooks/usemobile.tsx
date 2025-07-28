ce144a254725312e1c8c62241459e3d0dce3c539
Toulouse790 <cstophe0@gmail.com>
2025-07-28 15:49:23 +0200

    Update usemobile.tsx

--- client/src/hooks/usemobile.tsx
+++ client/src/hooks/usemobile.tsx
@@ -1,19 +1,15 @@
-import * as React from "react"
+import { useEffect, useState } from 'react';
 
-const MOBILE_BREAKPOINT = 768
+export function useMobile(breakpoint = 768): boolean {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < breakpoint);
 
-export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < breakpoint);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [breakpoint]);
 
-  React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
-
-  return !!isMobile
+  return isMobile;
 }
+
+export default useMobile;
