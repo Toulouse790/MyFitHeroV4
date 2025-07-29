616ec4c2f733c9944895482e5af5237dcd77ca7a
Toulouse790 <cstophe0@gmail.com>
2025-07-29 16:52:44 +0200

    Delete client/src/data/conversationalFlow_fixed.ts

--- client/src/data/conversationalFlow_fixed.ts
+++
@@ -1,8 +0,0 @@
-
-// Ajout en haut, à côté des autres fonctions :
-function resolveNextStepPack(packId: string): string {
-  return packId === 'custom' ? 'module_selection' : 'personal_info';
-}
-
-
-# => Le contenu complet avec corrections a été injecté ici par l'outil. Vous pouvez remplacer votre fichier.
