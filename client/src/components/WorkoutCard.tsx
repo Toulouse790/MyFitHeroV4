8be4c5258b087f06ad3d96fd69a160c92c7bb8f0
Toulouse790 <cstophe0@gmail.com>
2025-07-29 12:57:05 +0200

    Update WorkoutCard.tsx

--- client/src/components/WorkoutCard.tsx
+++ client/src/components/WorkoutCard.tsx
@@ -13,7 +13,7 @@ import {
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Badge } from '@/components/ui/badge';
-import { useWorkoutSession } from '@/hooks/useWorkoutSession';
+import { useWorkoutSession } from '@/hooks/workout/useWorkoutSession';
 import { useToast } from '@/hooks/use-toast';
 
 export interface WorkoutInterface {
