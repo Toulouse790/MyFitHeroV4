ed4aa836ca1cc57108bb6d92863ce25d340e474c
Toulouse790 <cstophe0@gmail.com>
2025-07-29 07:59:28 +0200

    Create RecoveryPage.tsx

---
+++ client/src/pages/RecoveryPage.tsx
@@ -0,0 +1,10 @@
+// pages/RecoveryPage.tsx
+import { MuscleRecoveryDashboard } from '@/components/MuscleRecoveryDashboard';
+
+export const RecoveryPage = () => {
+  return (
+    <div className="container mx-auto px-4 py-6">
+      <MuscleRecoveryDashboard />
+    </div>
+  );
+};
