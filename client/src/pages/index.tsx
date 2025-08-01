70efde6664f09847c133acebb23e6f0838b1d227
Toulouse790 <cstophe0@gmail.com>
2025-08-01 08:05:06 +0200

    Update index.tsx

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const IndexPage = () => {
-  return <h1 style={{color: 'red', textAlign: 'center', marginTop: '50px'}}>TEST UNIQUE</h1>;
+  return (
+    <div style={{
+      minHeight: '100vh', 
+      display: 'flex', 
+      alignItems: 'center', 
+      justifyContent: 'center',
+      backgroundColor: '#f0f0f0'
+    }}>
+      <h1 style={{color: 'red'}}>🚨 TEST ISOLATION - Page fonctionnelle</h1>
+    </div>
+  );
 };
 
 export default IndexPage;
